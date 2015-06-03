39765868d797280f6981b577d3291820ef5c661f
AL37F13 <lucero.rc.16@gmail.com>
2015-06-03 14:54:54 -0500

    dos
    agregamos mas plugins

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,13 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	var basedatos=window.sqlitePlugin.openDatabase({name: "coloresBD.db",createFromLocation:1});
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('B1', 'audio/C.mp3',function(){},function(msg){alert("error "+msg);});
+	audio.preloadFX('B2', 'audio/D.mp3',function(){},function(msg){alert("error "+msg);});
+	audio.preloadFX('B3', 'audio/E.mp3',function(){},function(msg){alert("error "+msg);});
+	audio.preloadFX('B4', 'audio/F.mp3',function(){},function(msg){alert("error "+msg);});
+	
 	$('#btnjugar').on('tap', function(){ 
 		var pantalla=$.mobile.getScreenHeight();
 		var encabezado=$('.ui-header').outerHeight();
@@ -18,6 +25,7 @@ document.addEventListener("deviceready",function(){
 	});
 	function quien (q)
 	{
+		audio.play(q);
 		return q.substring(1);
 	}
 	
