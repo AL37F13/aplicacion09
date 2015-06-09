d2c873a22344cf5d91a7d3b7be083ea15a1e2a95
AL37F13 <lucero.rc.16@gmail.com>
2015-06-09 13:23:10 -0500

    cinco
    modificacion

--- js/acciones.js
+++ js/acciones.js
@@ -38,7 +38,16 @@ function cargarnombrejugador ()
 		audio.play(q);
 		return q.substring(1);
 	}
-	
+	$('#btnconfigurar').on('tap',function(){
+		$('#txtnombre').val($('#jugador').text());
+	});
+	$('#btnguardar').on('tap',function (){
+		var nuevonombre=$('#txtnombre').val();
+		basedatos.transaction(function( consulta){
+		consulta.executeSql("UPDATE Usuario SET NombreUsuario =? WHERE ClaveUsuario='1';",[nuevonombre]);
+		});
+		cargarnombrejugador()
+	});
 }); 
 });
 
